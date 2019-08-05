5bacdcbcbffc148ce6fa80a69d3af6cb15de494e
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-08-05 10:12:37 +0900

    feat:firebaseのconfigファイルを作成

---
+++ src/firebase_config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig: {[k: string]: string} = {
+  apiKey: "AIzaSyC44AccnDFIYLIrHhEOXVrcdrM6vxAT9b8",
+  authDomain: "vue-firebase-app-52cbd.firebaseapp.com",
+  databaseURL: "https://vue-firebase-app-52cbd.firebaseio.com",
+  projectId: "vue-firebase-app-52cbd",
+  storageBucket: "vue-firebase-app-52cbd.appspot.com",
+  messagingSenderId: "717488106744",
+  appId: "1:717488106744:web:1ab3f1d927cd78fe"
+}
--- src/views/home/script.ts
+++ src/views/home/script.ts
@@ -2,5 +2,5 @@ import { Component, Vue } from "vue-property-decorator"
 
 @Component({})
 export default class Home extends Vue {
-  public title: string = "Hello Vue!!!"
+  public title: string = "firestore Read/Write App"
 }
