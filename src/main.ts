15cd921acae50f114b6e86b4964741ba8c603794
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-08-05 17:34:58 +0900

    feat:サインインしてないとsigninページにリダイレクトするように対応

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,30 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import "./registerServiceWorker"
+import firebase from "firebase"
 
 Vue.config.productionTip = false
+
+// 認証確認
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record: any) => record.meta.requiredAuth)) {
+    firebase.auth().onAuthStateChanged((user: firebase.User) => {
+      if (user) {
+        console.log("authorized")
+        next()
+      } else {
+        console.log("not authorized")
+        next({
+          path: "signin",
+          query: {
+            redirect: to.fullPath
+          }
+        })
+      }
+    })
+  }
+  next()
+})
 
 new Vue({
   router,
