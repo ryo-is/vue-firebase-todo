f98825804bd0ed33aef5e0e09d81567ada610d76
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-08-05 14:01:49 +0900

    feat:サインアウト処理を実装

--- src/views/home/script.ts
+++ src/views/home/script.ts
@@ -1,7 +1,17 @@
 import { Component, Vue } from "vue-property-decorator"
+import router from "@/router"
 import firebase from "firebase"
 
 @Component({})
 export default class Home extends Vue {
   public title: string = "firestore Read/Write App"
+
+  public async signOut(): Promise<void> {
+    try {
+      await firebase.auth().signOut()
+      router.push("/signin")
+    } catch (err) {
+      console.error(err)
+    }
+  }
 }
