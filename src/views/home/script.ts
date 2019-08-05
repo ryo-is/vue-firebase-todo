875c3b2bb9cce0d634d2a22b9d7e4265656a436a
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-08-05 17:23:25 +0900

    feat:updateできるように対応

--- src/views/home/script.ts
+++ src/views/home/script.ts
@@ -7,9 +7,10 @@ import { DataTableHeaderType, UserDataType } from "@/types"
 @Component({})
 export default class Home extends Vue {
   public title: string = "firestore Read/Write App"
+  public buttonText: string = "Create"
   public name: string = ""
   public age: number = 18
-  public gender: string = ""
+  public gender: "male" | "female" | "other" = "male"
   public genderItems: string[] = ["male", "female", "other"]
 
   public tableHeaders: DataTableHeaderType[] = [
@@ -23,15 +24,20 @@ export default class Home extends Vue {
     await this.getUser()
   }
 
+  // ユーザー全件取得
   public async getUser(): Promise<void> {
-    this.userData = []
-    const user: firebase.firestore.QuerySnapshot = await fireStore.collection("users").get()
-    user.docs.forEach((doc: firebase.firestore.QueryDocumentSnapshot) => {
-      console.log(doc.data())
-      this.userData.push(doc.data() as UserDataType)
-    })
+    try {
+      this.userData = []
+      const user: firebase.firestore.QuerySnapshot = await fireStore.collection("users").get()
+      user.docs.forEach((doc: firebase.firestore.QueryDocumentSnapshot) => {
+        this.userData.push(doc.data() as UserDataType)
+      })
+    } catch (err) {
+      console.error(err)
+    }
   }
 
+  // ユーザー作成 / 更新
   public async setUsers(): Promise<void> {
     try {
       await fireStore.collection("users").doc(this.name).set({
@@ -39,10 +45,27 @@ export default class Home extends Vue {
         age: this.age,
         gender: this.gender
       })
+      this.inputInit()
+      this.getUser()
       console.log("success!!!")
     } catch (err) {
       console.error(err)
     }
+  }
+
+  // 各項目の初期化
+  public inputInit(): void {
+    this.name = ""
+    this.age = 18
+    this.gender = "male"
+    this.buttonText = "Create"
+  }
+
+  public clickRow(item: UserDataType) {
+    this.name = item.name
+    this.age = item.age
+    this.gender = item.gender
+    this.buttonText = "Update"
   }
 
   // サインアウト処理
