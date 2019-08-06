f1f8d97949759bb2c3b53b79adab84d659a18f66
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-08-06 16:02:49 +0900

    feat:タスクの取得/作成/更新を実装

--- src/views/home/script.ts
+++ src/views/home/script.ts
@@ -8,29 +8,71 @@ import { TaskType } from "@/types"
 export default class Home extends Vue {
   public title: string = "firestore Read/Write App"
   public newTask: string = ""
-  public tasks: TaskType[] = [
-    { text: "hoge", done: false },
-    { text: "foobar", done: false }
-  ]
+  public tasks: TaskType[] = []
 
   get remainingTasks(): number {
     return this.tasks.length - this.completedTasks
-  }
-
-  get progress(): number {
-    return this.completedTasks / this.tasks.length * 100
   }
 
   get completedTasks(): number {
     return this.tasks.filter((task: TaskType) => task.done).length
   }
 
+  get progress(): number {
+    return this.completedTasks / this.tasks.length * 100
+  }
+
+  public async created(): Promise<void> {
+    await this.getTasks()
+  }
+
+  // タスク取得
+  public async getTasks(): Promise<void> {
+    try {
+      this.tasks = []
+      const taskData: firebase.firestore.QuerySnapshot = await fireStore.collection("tasks").get()
+      taskData.docs.forEach((doc: firebase.firestore.QueryDocumentSnapshot) => {
+        this.tasks.push({
+          id: doc.id,
+          text: doc.data().text,
+          done: doc.data().done
+        })
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  // タスク作成
   public async createTask(): Promise<void> {
-    this.tasks.push({
-      text: this.newTask,
-      done: false
-    })
-    this.newTask = ""
+    try {
+      const addTask: firebase.firestore.DocumentReference = await fireStore.collection("tasks").add({
+        text: this.newTask,
+        done: false
+      })
+      console.log(addTask)
+      this.tasks.push({
+        id: addTask.id,
+        text: this.newTask,
+        done: false
+      })
+      this.newTask = ""
+      console.log("success!!!")
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  // タスク更新
+  public async updateTask(task: any): Promise<void> {
+    try {
+      await fireStore.collection("tasks").doc(task.id).update({
+        done: task.done
+      })
+      console.log("success!!!")
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   // サインアウト処理
